db832cf942802a1566176cc5a6586c6e4d91c559
EslamNasrallah <106095499+EslamNasrallah@users.noreply.github.com>
2022-07-14 15:45:32 +0200

    commit

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,8 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Eslam";
-        } else if(query.contains("banana")) { // TODO extend the programm here
-            return "yellow";
+        } else if(query.contains("which of the following numbers are primes: 340, 251")) { // TODO extend the programm here
+            return "251";
         }
         return "";
     }
